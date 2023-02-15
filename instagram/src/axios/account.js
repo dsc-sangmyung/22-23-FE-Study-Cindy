c3c4caaea8afc5a63fa9f96ea015412d0ab1d550
leesoyuun <leesoyun4206@naver.com>
2023-02-15 20:50:23 +0900

    Fixed to include an email error message when an email error occurs and a password error message when a password error occurs

--- instagram/src/axios/account.js
+++ instagram/src/axios/account.js
@@ -5,9 +5,7 @@ function account(){
     const user_name = document.getElementById('user_name').value;
     const password = document.getElementById('password').value;
 
-    checkID(email);
-    checkPWd(password);
-
+    checkError(email,password)
     axios({
         method:"GET",
         url:'/api/account',
@@ -25,30 +23,39 @@ function account(){
 
 }
 
-function checkID(email){
-    let reg_email =  /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
-    let reg_number = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/
+function checkError(email,password){
     let error_msg = document.getElementById('error_msg');
+    error_msg.innerHTML = '';
 
-    if(reg_email.test(email) || reg_number.test(email)){
-        error_msg.innerHTML = '';
-        return email
-    }else{
+    if (!checkID(email)) {
         error_msg.className += "font-red mt-3 text-center";
         error_msg.innerText = 'Enter a valid email address.';
-        return false
+        return;
+    }
+
+    if (!checkPWd(password)) {
+        error_msg.className += "font-red mt-3 text-center";
+        error_msg.innerText = 'Enter a valid password.';
+        return;
     }
 }
 
-function checkPWd(pwd){
-    let error_msg = document.getElementById('error_msg');
+function checkID(email){
+    let reg_email =  /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
+    let reg_number = /^01([0|1|6|7|8|9])-?([0-9]{3,4})-?([0-9]{4})$/
 
-    if(pwd.toString().length<6){
-        error_msg.className += "font-red mt-3 text-center";
-        error_msg.innerText = 'Enter a valid password.';
-    }else{
-        error_msg.innerHTML = '';
-        return pwd;
-    }
+    if(reg_email.test(email) || reg_number.test(email))
+        return true;
+    else
+        return false;
+
+}
+
+function checkPWd(pwd){
+    if(pwd.toString().length<6)
+        return false;
+    else
+        return true;
+
 }
 export default account;
